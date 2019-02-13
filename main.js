e632f027fa923359b6d956ade402446c8fe8335c
TrippingZzz <46061647+TrippingZzz@users.noreply.github.com>
2019-02-12 19:55:27 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('#btn').click(function(){
+        var link = $('#link').val();
+        var username = $('#username').val();
+        var content = $('#content').val();
+        if (link==null || link=="",content==null || content=="")
+        {
+            alert("Please Fill Out All The Fields");
+            return false;
+        }
+        $.post(link, {"content": content, "username": username});
+
+    });
+});
